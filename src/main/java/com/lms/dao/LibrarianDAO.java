2750b212b60bb67562971e545b753d4d6d03247b
goulen <goulen2014@gmail.com>
2025-01-11 10:37:31 +0530

    feat: add new files - Librarian.java - LibrarianDAO.java

---
+++ src/main/java/com/lms/dao/LibrarianDAO.java
@@ -0,0 +1,105 @@
+package com.lms.dao;
+
+import com.lms.model.Librarian;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibrarianDAO {
+    private final String url = "jdbc:mysql://localhost:3306/librarymanagementsystem";
+    private final String user = "root";
+    private final String password = "admin_1";
+    
+    private static final String insertLibrarian = "insert into librarians(name, email, phone) values(?,?,?)";
+    private static final String viewLibrarian = "select id, name, email, phone from librarians where id=?";
+    private static final String viewAllLibrarians = "select * from librarians";
+    private static final String updateLibrarian = "update librarians set name=?, email=?, phone=? where id=?";
+    private static final String deleteLibrarian = "delect from librarians where id=?";
+ 
+    protected Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    
+    //add librarian
+    public void insertLibrarian(Librarian bean) {
+        try (Connection connection = getConnection();
+            PreparedStatement ps = connection.prepareStatement(insertLibrarian)) {
+            ps.setString(1, bean.getName());
+            ps.setString(2, bean.getEmail());
+            ps.setString(3, bean.getPhone());
+            ps.executeUpdate();            
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }        
+    }
+    //view a ibrarian
+    public Librarian getLibrarianById(int id) throws SQLException {
+        Librarian librarian = null;
+        try (Connection connection = getConnection();
+                PreparedStatement ps = connection.prepareStatement(viewLibrarian)) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                librarian = new Librarian(id, name, email, phone);
+            }            
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return librarian;
+    }
+    //view all librarians
+    public List<Librarian> getAllLibrarians() throws SQLException {
+        List<Librarian> librarians = new ArrayList<>();
+        try (Connection connection = getConnection();
+                PreparedStatement ps = connection.prepareStatement(viewAllLibrarians)) {
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                
+                librarians.add(new Librarian(id, name, email, phone));
+            }            
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return librarians;
+    }
+    //update librarian
+    public void update(Librarian librarian) throws SQLException {
+        try(Connection connection = getConnection();
+                PreparedStatement ps = connection.prepareStatement(updateLibrarian)) {
+            ps.setString(1, librarian.getName());
+            ps.setString(2, librarian.getEmail());
+            ps.setString(3, librarian.getPhone());
+            
+            ps.executeUpdate();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //delete librarian
+    public void delete(int id) throws SQLException {
+        try(Connection connection = getConnection();
+                PreparedStatement ps = connection.prepareStatement(deleteLibrarian)) {
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
