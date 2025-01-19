bf47c8b53167fd2394221515b5ab1bbdaa0309e9
goulen <goulen2014@gmail.com>
2025-01-19 22:12:06 +0530

    refactor: add functionality to BookDAO.java - getBookById() - updateBook()

--- src/main/java/com/lms/dao/BookDAO.java
+++ src/main/java/com/lms/dao/BookDAO.java
@@ -55,4 +55,42 @@ public class BookDAO {
         }
         return books;
     }
+    
+    private static final String selectBookById = "select * from books where id=?";
+    private static final String updateBook = "update books set title=?, author=?, quantity=? where id=?";
+    
+    //retrieve book by Id
+    public Book getBookById(int id) throws SQLException {
+        Book book = null;
+        try(Connection connection = DriverManager.getConnection(url, username, password);
+                PreparedStatement ps = connection.prepareStatement(selectBookById)) {
+            ps.setInt(1, id);
+            
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                String title = rs.getString("title");
+                String author = rs.getString("author");
+                int quantity = rs.getInt("quantity");
+                
+                book = new Book(title, author, quantity);
+                book.setId(id);
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return book;
+    }
+    //update book details
+    public void updateBook(Book book) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+                PreparedStatement ps = conn.prepareStatement(updateBook)) {
+            ps.setString(1, book.getTitle());
+            ps.setString(2, book.getAuthor());
+            ps.setInt(3, book.getQuantity());
+            
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
