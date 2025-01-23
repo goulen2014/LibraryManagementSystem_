ccdc65b063a1f3d5a3025b8733bfcf5999ec3cc7
goulen <goulen2014@gmail.com>
2025-01-23 17:36:26 +0530

    feat: add new file - AdminLoginServlet.java

---
+++ src/main/java/com/lms/controller/AdminLoginServlet.java
@@ -0,0 +1,32 @@
+
+package com.lms.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "AdminLoginServlet", urlPatterns = {"/AdminLoginServlet"})
+public class AdminLoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        PrintWriter out = response.getWriter();
+        
+        if(username.equals("UserName") && password.equals("PassWord")) {
+            HttpSession session = request.getSession();
+            session.setAttribute("name", username);
+            response.sendRedirect("adminDashboard.jsp");
+        } else {
+            out.println("Invalid username or password!");
+            RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+            rd.include(request, response);
+        }
+    }
+}
